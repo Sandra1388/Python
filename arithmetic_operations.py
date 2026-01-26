ed98748d6acef88e79eee5cde3756636b592448f
Sandra1388 <sandrabeena138@gmail.com>
2026-01-26 10:45:34 +0530

    new

---
+++ arithmetic_operations.py
@@ -0,0 +1,21 @@
+# Arithmentic operation
+# python is a dynamically typed language, i.e no need to define datatype
+# c, java, c++ are statically typed language
+
+a = 3
+b = 4
+
+sum = a + b
+d = b / a #division in python is float division
+dd = b // a #gives integer quotient only
+
+print("sum =", sum)
+print("Difference =", a - b)
+print("Product =", a * b)
+print("Division =", d)
+print("Division =", dd)
+
+print("A power of B =",a ** b) #a to the power of b
+
+print(type(b))  # to get the type of b
+print(type(d))  # to get the type of d
