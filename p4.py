2416ad3f3e585d538c8d381bfbec0e90c83fe4c6
Sandra1388 <sandrabeena138@gmail.com>
2026-01-27 19:40:22 +0530

    new

---
+++ p4.py
@@ -0,0 +1,13 @@
+# Control Statement
+# Conditional Statement [if, if-else, if elif... else]
+# Looping Statements [entry control and exit control]
+# Jumping Statements [break, continue, pass]
+
+a = int(input())
+b = int(input())
+if(a<b):
+    print('A is less than B')
+elif(a>b):
+    print('B is less than A')
+else:
+    print("A equal to B")
